a12ea184dc5d767585fa604b29c3e2a35f2a1288
Mraj23 <rajmehta231@gmail.com>
2023-02-24 15:45:09 -0500

    added metres stuff

--- example_9.py
+++ example_9.py
@@ -79,6 +79,10 @@ class GetVoiceCommands:
                 if s.isnumeric():
                     translation = int(s)
         
+        if 'm' not in self.command_list or 'M' not in self.command_list:
+            translation = translation/100
+
+        
         inc = {'rad': self.small_rad, 'translate': translation}
         
         return inc
