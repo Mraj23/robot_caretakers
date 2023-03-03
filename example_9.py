04d9e7c348d6e6c017e4532c2cdc28217fd2fde2
smallapplegirl <yizhao2@andrew.cmu.edu>
2023-03-03 16:46:28 -0500

    add capital face

--- example_9.py
+++ example_9.py
@@ -145,7 +145,7 @@ class GetVoiceCommands:
         """
         command = None
         if self.voice_command and self.command_list:
-            if ('base' in self.command_list) or ('face' in self.command_list):
+            if ('base' in self.command_list) or ('face' in self.command_list) or ('Face' in self.command_list):
                 if 'forward' in self.command_list:
                     command = {'joint': 'translate_mobile_base', 'inc': self.get_inc()['translate']}
                 if 'back' in self.command_list:
