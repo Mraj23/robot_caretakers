753fec0a39cf1e49b854b5cdb4c7979a0824b9fb
smallapplegirl <yizhao2@andrew.cmu.edu>
2023-04-03 13:28:09 -0400

    new change for json

--- example_9.py
+++ example_9.py
@@ -238,7 +238,7 @@ class VoiceTeleopNode(hm.HelloNode):
         :param command: A dictionary type.
         """
         joint_state = self.joint_state
-       
+
         if (joint_state is not None) and (command is not None):
 
             inc = command['inc']
@@ -289,7 +289,7 @@ class VoiceTeleopNode(hm.HelloNode):
             if joint_name == 'wrist_roll':
                 pose = {'joint_wrist_roll': new_value}
                 self.move_to_pose(pose)
-            
+
             if 'save' in command:
                 print(type(joint_state))
                 print(joint_state)
@@ -299,18 +299,18 @@ class VoiceTeleopNode(hm.HelloNode):
                 print('position ***************************')
                 print(joint_state.position)
                 print(type(joint_state.position))
-                
+
                 joint_state_dict = dict()
                 for idx, elem in enumerate(joint_state.name):
-                    joint_state_dict[elem] = joint_state.position[idx] 
-                
+                    joint_state_dict[elem] = joint_state.position[idx]
+
                 json_object = json.dumps(joint_state_dict)
 
                 # Writing to sample.json
                 name = command['save']
-                with open(f'/poses/{name}.json', "w") as outfile:
+                with open(f'poses/{name}.json', "w") as outfile:
                     outfile.write(json_object)
-                    
+
 
     def main(self):
         """
