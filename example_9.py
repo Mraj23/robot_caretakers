0662ffde48f3519823a25e240ae1439dd79044fa
smallapplegirl <yizhao2@andrew.cmu.edu>
2023-04-10 13:52:03 -0400

    keep moving flag

--- example_9.py
+++ example_9.py
@@ -176,6 +176,7 @@ class GetVoiceCommands:
 
 
 
+            if self.keep_moving_flag: return None
 
             if ("keep" in self.command_list) and ("moving" in self.command_list):
                 self.keep_moving_flag = True
